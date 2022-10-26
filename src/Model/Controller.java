9afbb18a81d7e62481c0810051f7ec14102cd6fc
KevinVincent016 <103274956+KevinVincent016@users.noreply.github.com>
2022-10-26 18:15:09 -0500

    Create Controller.java

---
+++ src/Model/Controller.java
@@ -0,0 +1,19 @@
+package Model;
+
+import java.util.ArrayList;
+
+public class Controller {
+
+    ArrayList<City> cities;
+    ArrayList<Country> countries;
+
+    public Controller(){
+        cities = new ArrayList<>();
+        countries = new ArrayList<>();
+    }
+
+    public void insert(String command){
+
+    }
+
+}
